639350104a74684d32ec0356435db80d691e59c2
TanzidAnan <tanzid.anan125@gmail.com>
2024-09-13 23:03:21 +0600

    recat setUp

---
+++ react_project/src/assets/compuntent/Compent.jsx
@@ -0,0 +1,11 @@
+
+
+const Compent = () => {
+    return (
+        <div>
+            <h1>Hello</h1>
+        </div>
+    );
+};
+
+export default Compent;
--- react_project/src/main.jsx
+++ react_project/src/main.jsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import Compent from './assets/compuntent/Compent.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    <Compent></Compent>
   </StrictMode>,
 )
